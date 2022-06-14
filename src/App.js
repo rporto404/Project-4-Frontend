597e3b939a966a4276d0ac6cf33aa9cbe0e54dce
chriselian8 <chriselian8@gmail.com>
2022-06-14 10:38:56 -0400

    basic crud functionality

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.svg'
+import './App.css'
+import React, {useState, useEffect} from 'react'
+import axios from 'axios'
+import Add from './components/Add.js'
+import Edit from './components/Edit.js'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+   let [books, setBooks] = useState([])
+
+   const getBooks = () => {
+      axios
+         .get('https://floating-fortress-76589.herokuapp.com/api/books')
+         .then(
+            (response) => setBooks(response.data),
+            (err) => console.log(err)
+         )
+         .catch((error) => console.log(error))
+   }
+   const handleCreate = (addBook) => {
+      axios
+         .post('https://floating-fortress-76589.herokuapp.com/api/books', addBook)
+         .then((response) => {
+            console.log(response)
+            getBooks()
+         })
+   }
+   const handleDelete = (event) => {
+      axios
+         .delete('https://floating-fortress-76589.herokuapp.com/api/books/' + event.target.value)
+         .then((response) => {
+            getBooks()
+         })
+   }
+   const handleUpdate = (editBook) => {
+      axios
+         .put('https://floating-fortress-76589.herokuapp.com/api/books/' + editBook.id, editBook)
+         .then((response) => {
+            getBooks()
+         })
+   }
+
+   useEffect(() => {
+      getBooks()
+   }, [])
+
+   return (
+      <>
+         <h1>Books</h1>
+         {books.map((book) => {
+            return (
+               <div className="book" key={book.id}>
+                  <h2>{book.title}</h2>
+                  <h3>{book.author}</h3>
+                  <h3>{book.genre}</h3>
+                  <h3>{book.year}</h3>
+                  <h3>{book.publisher}</h3>
+
+                  <button onClick={handleDelete} value={book.id}>
+                  X
+                  </button>
+               </div>
+            )
+         })}
+      </>
+   )
 }
-
 export default App;
